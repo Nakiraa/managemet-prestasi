ce40f85c446e6f21c23f36632c0c987a9d2c26e8
Agusputra2004 <raffyputriraffyputri39@gmail.com>
2023-02-27 11:09:48 +0700

    add part 25

--- database/migrations/2023_01_24_120201_create_materis_table.php
+++ database/migrations/2023_01_24_120201_create_materis_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMateriTable extends Migration
+class CreateMaterisTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,7 @@ class CreateMateriTable extends Migration
     {
         Schema::create('materi', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string ('judul_materi');
+            $table->string('judul_materi');
             $table->text('slug');
             $table->text('link');
             $table->integer('playlist_id');
